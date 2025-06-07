438aefbd0a6d76b079357fae1573823859fd7110
Intisar <intisarwaheed5@gmail.com>
2025-06-06 22:41:10 -0500

    For Work for Windows

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main(){
-    std::cout <<"Hello World" << std::endl;
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+    window.clear(sf::Color::Blue);
+    window.display();
+    sf::sleep(sf::seconds(2));
+    return 0;
 }
